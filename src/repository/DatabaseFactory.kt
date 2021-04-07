3337780de4f4169423b7392350b0d13e75763c36
SuperSk <samarashu02@gmail.com>
2021-04-07 11:57:54 +0530

    Added dependencies, environment variables and database factory

---
+++ src/repository/DatabaseFactory.kt
@@ -0,0 +1,40 @@
+package com.example.repository
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+
+
+    fun init(){
+        Database.connect(hikari())
+
+
+    }
+
+
+
+
+    fun hikari(): HikariDataSource {
+        val config = HikariConfig()
+        config.driverClassName = System.getenv("JDBC_DRIVER") // 1
+        config.jdbcUrl = System.getenv("DATABASE_URL") // 2
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        config.validate()
+
+        return HikariDataSource(config)
+    }
+
+
+    suspend fun <T> dbQuery(block: () -> T): T =
+        withContext(Dispatchers.IO) {
+            transaction { block() }
+        }
+
+}
